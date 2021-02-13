97e7a9d8dd77c482c6dbbfca30c8943a22d554cf
Rene Wang <rene.wang1@hotmail.com>
2021-02-13 14:30:36 -0500

    add register component

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,3 +1,100 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { BrowserRouter as Router } from "react-router-dom";
+import history from "../history";
+
+import Button from "./Button";
+
 export default function Register(props) {
-  return <h1>I am Register</h1>;
+  const [state, setState] = useState({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const handleSubmit = function (event) {
+    event.preventDefault();
+    if (state.password !== state.passwordConfirm) {
+      console.log("Passwords do not match");
+      return;
+    }
+    const params = {
+      username: state.username,
+      email: state.email,
+      password: state.password,
+    };
+    console.log(params);
+    history.push("/");
+    // return axios.post(`/api/login`, params).then((res) => {
+    //   if (res.data.code === 200) {
+    //     <Redirect to="/login" />;
+    //   } else {
+    //     return;
+    //   }
+    // });
+  };
+
+  return (
+    <Router>
+      <div className="login-container">
+        <form action="/login" method="POST" onSubmit={handleSubmit}>
+          <div className="login-credential">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              required
+              placeholder="Please enter username"
+              onChange={(event) => {
+                setState({ ...state, username: event.target.value });
+              }}
+            />
+          </div>
+          <div className="email-credential">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              required
+              name="email"
+              placeholder="Please enter email"
+              onChange={(event) => {
+                setState({ ...state, email: event.target.value });
+              }}
+            />
+          </div>
+          <div className="password-credential">
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              id="password"
+              required
+              name="password"
+              placeholder="Please enter password"
+              onChange={(event) => {
+                setState({ ...state, password: event.target.value });
+              }}
+            />
+          </div>
+          <div className="password-credential">
+            <label htmlFor="password-confirm">Confirm Password</label>
+            <input
+              type="text"
+              id="password-confirm"
+              required
+              name="password-confirm"
+              placeholder="Please enter password again"
+              onChange={(event) => {
+                setState({ ...state, passwordConfirm: event.target.value });
+              }}
+            />
+          </div>
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </Router>
+  );
 }
