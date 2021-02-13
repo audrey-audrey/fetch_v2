28df5a1c961fd3c72193a4a60658dc10ec48a0ee
Audrey <audreyuu@gmail.com>
2021-02-12 22:25:30 -0500

    Add user seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,11 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri' 
 
+# User
 User.destroy_all
 
-User.create!(
-  name: "Alison",
-  email: "alisonhussey@gmail.com",
-  password: "password"
-)
+# Random dog photos
+response = open('https://dog.ceo/api/breeds/image/random').read
+photo = JSON.parse(response)["message"]
+
+10.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.email,
+    password: "password",
+    location: 'Canada',
+    dog_name: Faker::Creature::Dog.name,
+    primary_image: JSON.parse(open('https://dog.ceo/api/breeds/image/random').read)["message"],
+    bio: "random bio",
+    playful: Faker::Boolean.boolean(true_ratio: 0.5),
+    affectionate: Faker::Boolean.boolean(true_ratio: 0.5),
+    high_energy: Faker::Boolean.boolean(true_ratio: 0.5),
+    shy: Faker::Boolean.boolean(true_ratio: 0.5),
+    well_trained: Faker::Boolean.boolean(true_ratio: 0.5),
+    large: Faker::Boolean.boolean(true_ratio: 0.5)
+  )
+end
+
