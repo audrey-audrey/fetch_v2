75f939411377e217bf9d2b8dd1b772bfc56c98d6
Rene Wang <rene.wang1@hotmail.com>
2021-02-13 00:03:18 -0500

    add input fields and form, state for user/pass, handleSubmit function

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,3 +1,46 @@
+import React from "react";
+import { useState } from "react";
+
+import Button from "./Button";
+
 export default function Login(props) {
-  return <h1>I am Login</h1>;
+  const [state, setState] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = { username: state.username, password: state.password };
+    console.log(params);
+  };
+
+  return (
+    <div>
+      Login
+      <form>
+        <label for="username">Username:</label>
+        <input
+          type="text"
+          name="username"
+          placeholder="Please enter username"
+          onChange={(event) => {
+            setState({ ...state, username: event.target.value });
+          }}
+        />
+        <br></br>
+        <label for="password">Password:</label>
+        <input
+          type="text"
+          name="password"
+          placeholder="Please enter password"
+          onChange={(event) => {
+            setState({ ...state, password: event.target.value });
+          }}
+        />
+        <br></br>
+        <Button onClick={(event) => handleSubmit(event)}>Login</Button>
+      </form>
+    </div>
+  );
 }
