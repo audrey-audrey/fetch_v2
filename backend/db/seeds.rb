bae690727f583baf9ed915455eebb6371ceca546
Audrey <audreyuu@gmail.com>
2021-02-14 18:21:37 -0500

    update user seeds to include lat/lng

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,30 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri' 
+require 'geocoder'
 
 # Users
 User.destroy_all
 
-def genPostalCode
-  first = ['M', 'L'].shuffle.first
-  second = rand(10).to_s
-  third = ('A'..'P').to_a.sample
-  fourth = rand(10).to_s
-  fifth = ('A'..'P').to_a.sample
-  sixth = rand(10).to_s
+user_list = [
+  [ "60 Queen St W, Toronto", Geocoder.search("60 Queen St W, Toronto").first.coordinates[0], Geocoder.search("60 Queen St W, Toronto").first.coordinates[1]],
+  [ "111 Lombard St, Toronto", Geocoder.search("111 Lombard St, Toronto").first.coordinates[0], Geocoder.search("111 Lombard St, Toronto").first.coordinates[1] ],
+  [ "350 Victoria St, Toronto", Geocoder.search("350 Victoria St, Toronto").first.coordinates[0], Geocoder.search("350 Victoria St, Toronto").first.coordinates[1] ],
+  [ "600 University Ave, Toronto", Geocoder.search("600 University Ave, Toronto").first.coordinates[0], Geocoder.search("600 University Ave, Toronto").first.coordinates[1] ],
+  [ "66 Nassau St, Toronto", Geocoder.search("66 Nassau St, Toronto").first.coordinates[0], Geocoder.search("66 Nassau St, Toronto").first.coordinates[1] ]
+]
 
-  postalCode = first + second + third + fourth + fifth + sixth
-end
-
-2.times do
-  User.create(
+user_list.each do |location, lat, lng|
+  User.create!(
     name: Faker::Name.name,
     email: Faker::Internet.unique.email,
     password: "password",
-    location: genPostalCode,
+    location: location,
+    lat: lat,
+    lng: lng,
     dog_name: Faker::Creature::Dog.name,
     primary_image: JSON.parse(open('https://dog.ceo/api/breeds/image/random').read)["message"],
-    image_2: Faker::LoremFlickr.image(search_terms: ['person']),
     bio: Faker::Lorem.paragraph,
     playful: Faker::Boolean.boolean(true_ratio: 0.5),
     affectionate: Faker::Boolean.boolean(true_ratio: 0.5),
@@ -39,6 +38,7 @@ end
     large: Faker::Boolean.boolean(true_ratio: 0.5)
   )
 end
+
 # Conversations
 # Conversation.destroy_all
 # User.conversations.create(
