8e9b1c6af75423465a6c3a6cf3d44b5f9f45a830
Rene Wang <rene.wang1@hotmail.com>
2021-02-15 16:22:03 -0500

    add empty message component

---
+++ client/src/components/Message.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, forceUpdate } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
+import history from "../history";
+
+import "./Login.css";
+
+import Button from "./Button";
+
+export default function Message(props) {
+  return <div>Message</div>;
+}
