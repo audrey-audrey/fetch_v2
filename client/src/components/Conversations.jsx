4e80e557715da3343228a23e1f050198db00dea1
Rene Wang <rene.wang1@hotmail.com>
2021-02-15 12:48:55 -0500

    setState + axios working

--- client/src/components/Conversations.jsx
+++ client/src/components/Conversations.jsx
@@ -1,25 +1,78 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect, forceUpdate } from "react";
 import axios from "axios";
 import { BrowserRouter as Router } from "react-router-dom";
 import history from "../history";
 
-import Button from "./Button";
-
 import "./Login.css";
 
+import Button from "./Button";
+
 export default function Conversations(props) {
-  const getConversations = function () {
-    const current_user = { id: localStorage.getItem("token") };
-    axios
-      .get("/api/conversations", current_user)
-      .then((res) => console.log(res))
-      .catch((e) => console.log(e));
+  const [loading, setLoading] = useState(true);
+  const [state, setState] = useState({
+    conversations: [],
+    initiators: [],
+    recipients: [],
+  });
+
+  useEffect(() => {
+    const params = { id: 1 };
+    axios.get(`/api/conversations`, { params }).then((res) => {
+      console.log(res);
+      setState({
+        conversations: res.data.conversations,
+        initiators: res.data.initiators,
+        recipients: res.data.recipients,
+      });
+    });
+    setLoading(false);
+  }, []);
+
+  let filterRecipients = function (arr) {
+    let myId = 1;
+    return arr.filter((item) => item.id !== myId);
   };
 
+  let filterInitiators = function (arr) {
+    let myId = 1;
+    return arr.filter((item) => item.id !== myId);
+  };
+
+  const displayRecipients = function () {
+    if (!loading) {
+      let filteredRecipients = filterRecipients(state.recipients);
+      for (const item of filteredRecipients) {
+        return <li>{item.name}</li>;
+      }
+    }
+  };
+
+  const displayInitiators = function () {
+    if (!loading) {
+      let filteredInitiators = filterInitiators(state.initiators);
+      for (const item of filteredInitiators) {
+        return <li>{item.name}</li>;
+      }
+    }
+  };
+
+  let recipients = displayRecipients();
+  let initiators = displayInitiators();
   return (
     <Router>
-      <div></div>
+      <div>
+        Conversations
+        <div>
+          {loading ? (
+            <span>Loading</span>
+          ) : (
+            <ul>
+              {recipients}
+              {initiators}
+            </ul>
+          )}
+        </div>
+      </div>
     </Router>
   );
 }
