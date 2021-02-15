cb22fce41d2ea15d5c34d9b7a435b1e5eecab78f
alisonhussey <alisonhussey@gmail.com>
2021-02-15 21:04:40 +0000

    add image and login/sign up buttons

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -3,19 +3,30 @@ import { useState } from "react";
 import axios from "axios";
 import { BrowserRouter as Router } from "react-router-dom";
 import history from "../history";
-import { Menu } from "semantic-ui-react";
+import { Container, Image, Button, Link, Icon } from "semantic-ui-react";
 
-import Button from "./Button";
+// import Button from "./Button";
 
 export default function Homepage(props) {
   return (
     <Router>
-      <Menu>
-
-      
-      <div>HELLO</div>
-      </Menu>
+      <Container textAlign='center'>
+        <Image
+          src="https://thehappypuppysite.com/wp-content/uploads/2019/02/Australian-Shepherd-Temperament-long.jpg"
+          // size="large"
+          circular
+          fluid
+        />
+        <Button.Group>
+          <Button size="huge">
+            Log In
+          </Button>
+          <Button.Or />
+          <Button size="huge">
+            Sign Up
+          </Button>
+        </Button.Group>
+      </Container>
     </Router>
-  )
-
+  );
 }
