272d0cddfa1bf2e636798ce9566bf3352748c200
Rene Wang <rene.wang1@hotmail.com>
2021-02-15 03:43:12 +0000

    delete routes without controllers

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :users
+    resources :conversations, only: [:index, :create] do
+      resources :messages, only: [:index, :create]
+    end
   end
 end
