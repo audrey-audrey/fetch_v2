7227dee93b11ee0fe22dadae50e5825ca2abba26
Rene Wang <rene.wang1@hotmail.com>
2021-02-15 20:56:33 -0500

    add displayMessages function

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -8,6 +8,7 @@ import "./Login.css";
 import Button from "./Button";
 
 export default function Message(props) {
+  const [loading, setLoading] = useState(true);
   const [state, setState] = useState({
     messages: [],
   });
@@ -18,9 +19,30 @@ export default function Message(props) {
       .then((res) => {
         setState({ messages: res.data });
       });
+    setLoading(false);
   }, []);
 
-  const displayMessages;
+  const displayMessages = function () {
+    if (!loading) {
+      for (const item of state.messages) {
+        if (item.content) {
+          return (
+            <li>
+              <h4>{item.user_id}</h4>
+              <p>{item.content}</p>
+            </li>
+          );
+        }
+      }
+    }
+  };
 
-  return <div>Message</div>;
+  const messages = displayMessages();
+
+  return (
+    <div>
+      Messages
+      <div>{loading ? <span>Loading</span> : <ul>{messages}</ul>}</div>
+    </div>
+  );
 }
