e463ec46dc8be353ee8a4ec07afad2d7fcaf5df2
Rene Wang <rene.wang1@hotmail.com>
2021-02-15 20:38:51 -0500

    update index to correctly return message data

--- backend/app/controllers/api/messages_controller.rb
+++ backend/app/controllers/api/messages_controller.rb
@@ -1,23 +1,30 @@
 class Api::MessagesController < ApplicationController
 
-  def index 
-    
+  before_action do
+    @conversation = Conversation.find(params[:conversation_id])
+  end
+
+  def index
+    messages = @conversation.messages
+
+    messages.where("user_id != ? AND read = ?", params[:user_id], false).update_all(read: true)
+
+    message = @conversation.messages.new
+
+    render json: messages
   end
 
   def create
-    if Conversation.link(params[:initiator_id], params[:recipient_id]).present?
-      conversation = Conversation.link(params[:initiator_id], params[:recipient_id]).first
-    else
-      conversation = Conversation.create!(conversation_params)
+    message = @conversation.messages.new(message_params)
+    message.user = current_user
+
+    if message.save
+      redirect_to conversation_messages_path(@conversation)
     end
-
-    render json :conversation
   end
-  
-
 
   private
-  def conversation_params
-    params.permit(:initiator_id, :recipient_id)
-  end
+    def message_params
+      params.require(:message).permit(:body, :user_id)
+    end
 end
