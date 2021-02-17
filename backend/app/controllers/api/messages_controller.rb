69362f61c8850b839a5c1aaca115cb7f2d0523f5
Rene Wang <rene.wang1@hotmail.com>
2021-02-17 18:58:04 +0000

    refactor to include user entry with message

--- backend/app/controllers/api/messages_controller.rb
+++ backend/app/controllers/api/messages_controller.rb
@@ -1,23 +1,24 @@
 class Api::MessagesController < ApplicationController
 
   before_action do
-    @conversation = Conversation.find(params[:conversation_id])
+    @conversation = Conversation.includes(messages: [:user]).find(params[:conversation_id])
   end
 
   def index
     messages = @conversation.messages
+    puts messages.inspect
+    render json: {user_info: messages}, include: :user
 
-    messages.where("user_id != ? AND read = ?", params[:user_id], false).update_all(read: true)
+    # messages.where("user_id != ? AND read = ?", params[:user_id], false).update_all(read: true)
 
-    user_info = messages.select("messages.*, users.name as name").joins("INNER JOIN users ON messages.user_id = users.id")
+    # user_info = messages.select("messages.*, users.name as name").joins("INNER JOIN users ON messages.user_id = users.id")
 
-    render json: {messages: messages, user_info: user_info}
   end
 
   def create
-    message = @conversation.messages.create!(message_params)
+    message = @conversation.messages.includes(:user).create!(message_params)
 
-    render json: message
+    render json: message, include: :user
   end
 
   private
