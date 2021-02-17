ef359a8e570bca43e9e243ec0b0114898eaa94ee
Rene Wang <rene.wang1@hotmail.com>
2021-02-17 18:58:24 +0000

    change FK column to include _id

--- backend/app/models/conversation.rb
+++ backend/app/models/conversation.rb
@@ -1,10 +1,9 @@
 class Conversation < ApplicationRecord
-  belongs_to :recipient, class_name: "User", foreign_key: "recipient"
-  belongs_to :initiator, class_name: "User", foreign_key: "initiator"
+  belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
+  belongs_to :initiator, class_name: "User", foreign_key: "initiator_id"
   has_many :messages, dependent: :destroy
 
   scope :link, -> (initiator_id, recipient_id) do
-    where(
-      "(conversations.initiator_id = ? AND conversations.recipient_id = ?) OR (conversations.recipient_id = ? AND conversations.initiator_id = ?",initiator_id, recipient_id, initiator_id, recipient_id)
+    where("(conversations.initiator_id = ? AND conversations.recipient_id = ?) OR (conversations.recipient_id = ? AND conversations.initiator_id = ?)",initiator_id, recipient_id, initiator_id, recipient_id)
   end
 end
