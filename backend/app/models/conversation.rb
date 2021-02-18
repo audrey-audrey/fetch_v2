330d6921a6810a05d10fd1542fb8e72e83c9a34b
Rene Wang <rene.wang1@hotmail.com>
2021-02-18 21:21:22 +0000

    remove function in conversation model

--- backend/app/models/conversation.rb
+++ backend/app/models/conversation.rb
@@ -2,12 +2,4 @@ class Conversation < ApplicationRecord
   belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
   belongs_to :initiator, class_name: "User", foreign_key: "initiator_id"
   has_many :messages, dependent: :destroy
-  
-  def recipient(current_user)
-    self.sender_id == current_user.id ? self.receiver : self.sender
-  end
-
-  def unread_message_count(current_user)
-    self.messages.where("user_id != ? AND read = ?", current_user.id, false).count
-  end
 end
